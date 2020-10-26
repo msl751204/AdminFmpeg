1a6e6335e81a6bff9ec97d1e019f40af1ac5c52b
macmasl <29512989@qq.com>
2020-10-26 19:13:17 +0800

    新增加个新闻模块

---
+++ src/com/msb/knowmain.java
@@ -0,0 +1,14 @@
+package com.msb;
+
+/**
+ * @Auther: masl
+ * @Date: 2020/10/26 - 10 - 26 - 17:40
+ * @Description: com.msb
+ * @version: 1.0
+ */
+public class knowmain {
+    public static void main(String[] args){
+        System.out.println("first APpp");
+    }
+
+}
