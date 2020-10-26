87498575cf60d5b7053fa5426078a691f6e2cb5e
macmasl <29512989@qq.com>
2020-10-26 19:37:58 +0800

    新增加个person类。属性为年龄，姓名和性别

---
+++ src/com/msb/person.java
@@ -0,0 +1,13 @@
+package com.msb;
+
+/**
+ * @Auther: masl
+ * @Date: 2020/10/26 - 10 - 26 - 19:35
+ * @Description: com.msb
+ * @version: 1.0
+ */
+public class person {
+    private int age;
+    private String name;
+    private String sex;
+}
